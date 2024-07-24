068514ff961709f8ce0bd65a6b22546f21b837ae
josiane-silwa <108165656+josiane-silwa@users.noreply.github.com>
2024-07-23 23:57:56 -0300

    Construcao da pagina home

--- portfolio/src/pages/Home/index.jsx
+++ portfolio/src/pages/Home/index.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import NavBar from '../../components/NavBar'
 import Header from '../../components/Header'
 import Projetos from '../../components/Projetos'
-import Experiencias from '../../components/Experiencias'
+import Experiencias from '../../components/Experiencias/index'
 import Footer from '../../components/Footer'
+import SobreMim from '../../components/SobreMim'
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <Header />
+      <SobreMim />
       <Experiencias />
       <Projetos />
       <Footer />
