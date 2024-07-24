d9df9ad24697554e2cd5462f59e5f4f1f2fb64c0
josiane-silwa <108165656+josiane-silwa@users.noreply.github.com>
2024-07-23 23:56:57 -0300

    construção do componente referente aos projetos

--- portfolio/src/components/Projetos/index.jsx
+++ portfolio/src/components/Projetos/index.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import instagramIcon from '../../assets/instagram2.png'
+import './Projetos.modules.css'
 
 const Projetos = () => {
   return (
-    <div>
-      <h1>Projetos</h1>
+    <div className='container-projetos'>
+        <h1>Projetos</h1>
+      <div className='projetos-coluna'>
+        <div>
+          <div className='img-projeto'>Imagem projeto</div>
+          <h2>Nome do projeto</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          <h4>Tecnologias usadas no projeto</h4>
+          <div className='btn'>
+            <button className='btn1'><a href="#">Acessar Projeto</a></button>
+            <button className='btn2'><a href="#">Acessar Repositório</a></button>
+          </div>
+        </div>
+        <div>
+          <div className='img-projeto'>Imagem projeto</div>
+          <h2>Nome do projeto</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          <h4>Tecnologias usadas no projeto</h4>
+          <div className='btn'>
+            <button className='btn1'><a href="#">Acessar Projeto</a></button>
+            <button className='btn2'><a href="#">Acessar Repositório</a></button>
+          </div>
+        </div>
+      </div>
+      <div className='projetos-coluna'>
+        <div>
+          <div className='img-projeto'>Imagem projeto</div>
+          <h2>Nome do projeto</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          <h4>Tecnologias usadas no projeto</h4>
+          <div className='btn'>
+            <button className='btn1'><a href="#">Acessar Projeto</a></button>
+            <button className='btn2'><a href="#">Acessar Repositório</a></button>
+          </div>
+        </div>
+        <div>
+          <div className='img-projeto'>Imagem projeto</div>
+          <h2>Nome do projeto</h2>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          <h4>Tecnologias usadas no projeto</h4>
+          <div className='btn'>
+            <button className='btn1'><a href="#">Acessar Projeto</a></button>
+            <button className='btn2'><a href="#">Acessar Repositório</a></button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
