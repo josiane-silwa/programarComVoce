deafcd01fd6e7bb5fcdba8bb3d79b37349952442
josiane-silwa <108165656+josiane-silwa@users.noreply.github.com>
2024-07-23 23:58:46 -0300

    Arquivo contendo as informaçoes sobre as experiencias

---
+++ portfolio/src/data/data.js
@@ -0,0 +1,32 @@
+const experiencias = [
+    {
+        id : 1,
+        empresa: "INPE",
+        funcao: "Desenvolvedor Python",
+        descricao: "Desenvolvimento de rotinas computacionais para preparação e análise de dados e geração de graficos e mapas.",
+        competencias: "python" 
+    },
+    {
+        id : 2,
+        empresa: "Inpe",
+        funcao: "Cientista de dados",
+        descricao: "Desenvolvimento de rotinas computacionais para preparação e análise de dados e geração de graficos e mapas.",
+        competencias: "python, softwares de vizualização de dados" 
+    },
+    {
+        id : 3,
+        empresa: "Linea",
+        funcao: "Desenvolvedor Frontend",
+        descricao: "Desenvolvimento de web/apps.",
+        competencias: "html, css, javascript, react js" 
+    },
+    {
+        id : 4,
+        empresa: "Linea",
+        funcao: "Desenvolvedor Backend",
+        descricao: "Desenvolvimento de aplicações e APIs.",
+        competencias: "django, django rest framework" 
+    }
+]
+
+export default experiencias
