671786887073982af12c1d555e194b0e9dfb02bd
josiane-silwa <108165656+josiane-silwa@users.noreply.github.com>
2024-07-23 23:55:28 -0300

    construção do componente referente ao footer

--- portfolio/src/components/Footer/index.jsx
+++ portfolio/src/components/Footer/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Footer.modules.css'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className='footer'>
+      <p>
+        @ 2024 - Josiane da Silva
+      </p>
+      <p>
+        Powered by {}
+      </p>
     </div>
   )
 }
