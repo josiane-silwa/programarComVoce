a778ef1bad3a919ecbb176b4446551748be6af14
josiane-silwa <108165656+josiane-silwa@users.noreply.github.com>
2024-07-23 23:55:56 -0300

    construção do componente referente ao header

--- portfolio/src/components/Header/index.jsx
+++ portfolio/src/components/Header/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import image from '../../assets/cel06012017071.jpg'
+import './Header.modules.css'
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className='container-header'>
+      <div className='text'>
+        <p>Ol&aacute;, eu sou</p>
+        <h1>Josiane da Silva</h1>
+        <p>Desenvolvedora Web Full Stack</p>
+        <button>LinkedIn</button>
+      </div>
+      <div className='img'>
+        <img src={image} alt="" />
+      </div>
     </div>
   )
 }
